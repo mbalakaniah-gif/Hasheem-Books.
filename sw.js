4c50b078fe990d9a0c3739b4978b2b4ee0ef4376
mbalakaniah-gif <mbalakaniah@gmail.com>
2026-01-24 15:06:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'hasheem-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/1.png',
+  '/manifest.json'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
